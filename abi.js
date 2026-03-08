e260f773c79ca9b59d380e1557a27f9cf9a1da6a
alberthuellstrounk69-dotcom <alberthuellstrounk69@gmail.com>
2026-03-07 22:18:40 -0600

    Add files via upload

--- abi.js
+++ abi.js
@@ -1,80 +1,99 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
+const ethersABI = require("@ethersproject/abi");
+const difflib = require('difflib');
+
+class AbiUtils {
+
+  diff(orig={}, cur={}){
+    let plus = 0;
+    let minus = 0;
+
+    const unifiedDiff = difflib.unifiedDiff(
+      orig.humanReadableAbiList,
+      cur.humanReadableAbiList,
+      {
+        fromfile: orig.contractName,
+        tofile: cur.contractName,
+        fromfiledate: `sha: ${orig.sha}`,
+        tofiledate: `sha: ${cur.sha}`,
+        lineterm: ''
+      }
+    );
+
+    // Count changes (unified diff always has a plus & minus in header);
+    if (unifiedDiff.length){
+      plus = -1;
+      minus = -1;
     }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-const semver = __importStar(require("semver"));
-function update(compilerVersion, abi) {
-    let hasConstructor = false;
-    let hasFallback = false;
-    for (let i = 0; i < abi.length; i++) {
-        const item = abi[i];
-        if (item.type === 'constructor') {
-            hasConstructor = true;
-            // <0.4.5 assumed every constructor to be payable
-            if (semver.lt(compilerVersion, '0.4.5')) {
-                item.payable = true;
-            }
-        }
-        else if (item.type === 'fallback') {
-            hasFallback = true;
-        }
-        if (item.type !== 'event') {
-            // add 'payable' to everything, except constant functions
-            if (!item.constant && semver.lt(compilerVersion, '0.4.0')) {
-                item.payable = true;
-            }
-            // add stateMutability field
-            if (semver.lt(compilerVersion, '0.4.16')) {
-                if (item.payable) {
-                    item.stateMutability = 'payable';
-                }
-                else if (item.constant) {
-                    item.stateMutability = 'view';
-                }
-                else {
-                    item.stateMutability = 'nonpayable';
-                }
-            }
-        }
+
+    unifiedDiff.forEach(line => {
+      if (line[0] === `+`) plus++;
+      if (line[0] === `-`) minus++;
+    })
+
+    return {
+      plus,
+      minus,
+      unifiedDiff
     }
-    // 0.1.2 from Aug 2015 had it. The code has it since May 2015 (e7931ade)
-    if (!hasConstructor && semver.lt(compilerVersion, '0.1.2')) {
-        abi.push({
-            type: 'constructor',
-            payable: true,
-            stateMutability: 'payable',
-            inputs: []
-        });
+  }
+
+  toHumanReadableFunctions(contract){
+    const human = [];
+    const ethersOutput = new ethersABI.Interface(contract.abi).functions;
+    const signatures = Object.keys(ethersOutput);
+
+    for (const sig of signatures){
+      const method = ethersOutput[sig];
+      let returns = '';
+
+      method.outputs.forEach(output => {
+        (returns.length)
+          ? returns += `, ${output.type}`
+          : returns += output.type;
+      });
+
+      let readable = `${method.type} ${sig} ${method.stateMutability}`;
+
+      if (returns.length){
+        readable += ` returns (${returns})`
+      }
+
+      human.push(readable);
     }
-    if (!hasFallback && semver.lt(compilerVersion, '0.4.0')) {
-        abi.push({
-            type: 'fallback',
-            payable: true,
-            stateMutability: 'payable'
-        });
+
+    return human;
+  }
+
+  toHumanReadableEvents(contract){
+    const human = [];
+    const ethersOutput = new ethersABI.Interface(contract.abi).events;
+    const signatures = Object.keys(ethersOutput);
+
+    for (const sig of signatures){
+      const method = ethersOutput[sig];
+      const readable = `${ethersOutput[sig].type} ${sig}`;
+      human.push(readable);
     }
-    return abi;
+
+    return human;
+  }
+
+  generateHumanReadableAbiList(_artifacts, sha){
+    const list = [];
+    if (_artifacts.length){
+      for (const item of _artifacts){
+        const fns = this.toHumanReadableFunctions(item);
+        const evts = this.toHumanReadableEvents(item);
+        const all = fns.concat(evts);
+        list.push({
+          contractName: item.contractName,
+          sha: sha,
+          humanReadableAbiList: all
+        })
+      }
+    }
+    return list;
+  }
 }
-module.exports = {
-    update
-};
+
+module.exports = AbiUtils;
