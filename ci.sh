d670992fc3011181cd263b5a952514494c7555bb
alberthuellstrounk69-dotcom <alberthuellstrounk69@gmail.com>
2026-03-07 22:35:24 -0600

    Add files via upload

--- ci.sh
+++ ci.sh
@@ -1,21 +1,51 @@
 #!/usr/bin/env bash
 
-# Toggles optimizer on/off
-VIAR_IR=$VIA_IR
+set -o errexit
 
-# Minimize integration test output
-SILENT=true
+if [ "$TEST" = "integration" ]; then
 
-node --max-old-space-size=4096 \
-  ./node_modules/.bin/nyc \
-    --reporter=lcov \
-    --exclude '**/sc_temp/**' \
-    --exclude '**/test/**/' \
-    --exclude 'plugins/resources/matrix.js' \
-  -- \
-  mocha \
-    test/units/* test/integration/* \
-    --require "test/util/mochaRootHook.js" \
-    --timeout 100000 \
-    --no-warnings \
-    --exit \
+  npm test
+
+elif [ "$TEST" = "geth" ]; then
+
+  npx geth-dev-assistant \
+    --launch \
+    --tag 'latest' \
+    --accounts 4 \
+    --balance 100 \
+    --gasLimit 8000000
+
+  npm test
+  docker stop geth-client
+
+elif [ "$TEST" = "colony" ]; then
+
+  npm install -g yarn
+  git clone https://github.com/JoinColony/colonyNetwork.git
+  cd colonyNetwork || exit
+  yarn
+  yarn remove -W eth-gas-reporter --dev
+
+  env
+
+  SLUG="$TRAVIS_REPO_SLUG"
+  BRANCH="$TRAVIS_BRANCH"
+
+  if [ -n "$TRAVIS_PULL_REQUEST_SLUG" ]; then
+    SLUG="$TRAVIS_PULL_REQUEST_SLUG"
+  fi
+
+  if [ -n "$TRAVIS_PULL_REQUEST_BRANCH" ]; then
+    BRANCH="$TRAVIS_PULL_REQUEST_BRANCH"
+  fi
+
+  echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+  echo "TESTING BRANCH: https://github.com/$SLUG.git#$BRANCH"
+  echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+
+  yarn add -W https://github.com/"$SLUG".git#"$BRANCH"
+  git submodule update --init
+  yarn run provision:token:contracts
+  DEBUG_CODECHECKS_TABLE=true yarn run test:contracts:gasCosts
+
+fi
